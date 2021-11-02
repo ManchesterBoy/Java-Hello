9c30a16248a7835f492828db3f499982e0096948
Leslie <1341762426@qq.com>
2021-11-02 19:36:58 +0800

    111

---
+++ HelloW.java
@@ -0,0 +1,9 @@
+public class HelloW {
+
+    public  static void main(String[] args) {
+        System.out.println("Hello World!!!+123");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(" " + i);
+        }
+    }
+}
